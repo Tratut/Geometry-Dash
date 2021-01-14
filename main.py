d405c34bb565d3623b467f8d0f8a986d9bd917ce
Tratut <trataty111@gmail.com>
2021-01-14 21:30:43 +0300

    еределана структура программы

--- main.py
+++ main.py
@@ -2,45 +2,7 @@ import pygame
 from start_screen import start_screen
 
 
-def load_level(filename):
-    filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-
-    # и подсчитываем максимальную длину
-    max_width = max(map(len, level_map))
-
-    # дополняем каждую строку пустыми клетками ('.')
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
-def generate_level(level):
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
-            elif level[y][x] == '@':
-                Tile('empty', x, y)
-                new_player = Player(x, y)
-    # вернем игрока, а также размер поля в клетках
-    return new_player, x, y
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, x, y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = pygame.image.load(tile_type)
-        self.image = pygame.transform.scale(40, 40)
-        self.rect = self.image.get_rect()
-
-        self.add(tiles_group, all_sprites)
-
-
-class Game():
+class Cube():
     def __init__(self, screen, size):
         self.screen = screen
         self.size = size
@@ -49,25 +11,12 @@ class Game():
         self.y = 441
         self.wight = 40
         self.heigt = 40
-
+        self.speed = 3
         self.cubeSet = 0
-
         self.isJump = False
-        self.jumpCount = 10
-
+        self.jumpCount = 12
         self.main_cube = pygame.image.load('data\Main_cube\skin1.png')
         self.main_cube = pygame.transform.scale(self.main_cube, (40, 40))
-        # земля
-        self.g_w, self.g_h = self.size
-        self.bg = pygame.transform.smoothscale(pygame.image.load('data\Grounds\gr1.jpg'), (self.g_w, self.g_h))
-        self.pos_xg = 0
-        self.speed = 3
-
-        # фон
-        self.bg_w, self.bg_h = 800, self.y + self.heigt
-        self.g = pygame.transform.smoothscale(pygame.image.load('data\ofon.jpg'), (self.bg_w, self.bg_h))
-        self.pos_xbg = 0
-        self.speed_bg = 5
 
         # вращение
         self.rot = 0
@@ -76,12 +25,6 @@ class Game():
         self.rect = self.main_cube.get_rect()
         self.pos_rot = 0  # проверка позиции после вращения
         self.checker_rot = 0  # проверка того что вращение куба произошло 1 раз
-
-        # блоки
-        self.w_bl, self.h_nl = 40, 40
-        self.bl = pygame.transform.smoothscale(pygame.image.load('data\Grounds\gr1.jpg'), (self.w_bl, self.h_nl))
-        self.pos_xbl = 500
-        self.x_rel = 800
 
     def action(self, keys):
         """
@@ -96,7 +39,7 @@ class Game():
                     self.pos_rot = 0
                     self.checker_rot = 0
         else:
-            if self.jumpCount >= -10:
+            if self.jumpCount >= -12:
                 if self.jumpCount < 0:
                     self.y += (self.jumpCount ** 2) // 4 - 1
                 else:
@@ -104,7 +47,7 @@ class Game():
                 self.jumpCount -= 1
             else:
                 self.isJump = False
-                self.jumpCount = 10
+                self.jumpCount = 12
                 self.y -= 1
         pygame.display.update()
 
@@ -120,6 +63,45 @@ class Game():
         else:
             self.rotate()
         pygame.display.update()
+
+    def rotate(self):
+        """
+        Вращение куба во время прыжка
+        """
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot - self.rot_speed) % 360
+            image = pygame.transform.rotate(self.main_cube, self.rot)
+            screen.blit(image, (self.x, self.y))
+
+            old_center = self.rect.center
+            new_image = image
+            self.rect = new_image.get_rect()
+            self.rect.center = old_center
+        pygame.display.update()
+
+
+class Location():
+    def __init__(self, screen, size):
+        self.screen = screen
+        self.size = size
+        # гг
+        self.x = 200
+        self.y = 441
+        self.wight = 40
+        self.heigt = 40
+        # земля
+        self.g_w, self.g_h = self.size
+        self.bg = pygame.transform.smoothscale(pygame.image.load('data\Grounds\gr1.jpg'), (self.g_w, self.g_h))
+        self.pos_xg = 0
+        self.speed = 3
+
+        # фон
+        self.bg_w, self.bg_h = 800, self.y + self.heigt
+        self.g = pygame.transform.smoothscale(pygame.image.load('data\ofon.jpg'), (self.bg_w, self.bg_h))
+        self.pos_xbg = 0
+        self.speed_bg = 5
 
     def draw_bg(self):
         """
@@ -146,22 +128,14 @@ class Game():
         screen.blit(self.g, (x_rel, 0))
         screen.blit(self.g, (x_part2, 0))
 
-    def rotate(self):
-        """
-        Вращение куба во время прыжка
-        """
-        now = pygame.time.get_ticks()
-        if now - self.last_update > 50:
-            self.last_update = now
-            self.rot = (self.rot - self.rot_speed) % 360
-            image = pygame.transform.rotate(self.main_cube, self.rot)
-            screen.blit(image, (self.x, self.y))
 
-            old_center = self.rect.center
-            new_image = image
-            self.rect = new_image.get_rect()
-            self.rect.center = old_center
-        pygame.display.update()
+class Block():
+    def __init__(self):
+        self.w_bl, self.h_nl = 40, 40
+        self.bl = pygame.transform.smoothscale(pygame.image.load('data\Grounds\gr1.jpg'), (self.w_bl, self.h_nl))
+        self.pos_xbl = 500
+        self.x_rel = 800
+        self.speed = 3
 
     def block(self, i):
         self.pos_xbl -= self.speed
@@ -177,11 +151,11 @@ def start():
     """
     Запуск игры и др ее компонентов
     """
-    game.action(keys)
-    game.draw_bg()
-    game.draw_character()
-    game.generate_blocK()
-    # render()
+    cub.action(keys)
+    loc.draw_bg()
+    cub.draw_character()
+    blck.generate_blocK()
+    render()
     # screen.fill((0, 0, 0))
 
 
@@ -200,7 +174,9 @@ if __name__ == "__main__":
     clock = pygame.time.Clock()
     size = (800, 600)
     screen = pygame.display.set_mode(size)
-    game = Game(screen, size)
+    cub = Cube(screen, size)
+    loc = Location(screen, size)
+    blck = Block()
     run = True
     if start_screen(screen, clock):
         while run:
